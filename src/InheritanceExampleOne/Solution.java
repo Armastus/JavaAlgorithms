152f322a727815b7cbde64aee2889f92c08ac0e6
david <david.o.bustillos@gmail.com>
2018-05-04 16:07:49 -0700

    Adding inheritance example

--- src/InheritanceExampleOne/Solution.java
+++ src/InheritanceExampleOne/Solution.java
@@ -1,4 +1,4 @@
-package Inheritance;
+package InheritanceExampleOne;
 
 import java.util.Scanner;
 
--- src/InheritanceExampleOne/Student.java
+++ src/InheritanceExampleOne/Student.java
@@ -1,4 +1,4 @@
-package Inheritance;
+package InheritanceExampleOne;
 
 class Student extends Person{
     /*
