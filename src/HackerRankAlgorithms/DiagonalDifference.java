63037d93790c10126b586eba3aa25bd4da7c4664
Armastus <david.o.bustillos@gmail.com>
2018-07-05 12:20:22 -0700

    just more stuff to learn

--- src/HackerRankAlgorithms/DiagonalDifference.java
+++ src/HackerRankAlgorithms/DiagonalDifference.java
@@ -15,8 +15,9 @@ public class DiagonalDifference {
         int primaryDiagonal = 0;
         int secondaryDiagonal = 0;
         int arrLength = a.length;
+        int indexLength = arrLength - 1;
 
-        for (int ii = 0, indexLength = arrLength - 1; ii < arrLength; ii++) {
+        for (int ii = 0; ii < arrLength; ii++) {
             primaryDiagonal += a[ii][ii]; //11
             secondaryDiagonal += a[ii][indexLength]; //4
             indexLength -= 1;
@@ -24,34 +25,12 @@ public class DiagonalDifference {
         return Math.abs(primaryDiagonal - secondaryDiagonal);
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[][] a = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            String[] aRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < n; j++) {
-                int aItem = Integer.parseInt(aRowItems[j]);
-                a[i][j] = aItem;
-            }
-        }
+    public static void main(String[] args) {
+        int[][] a = {{11, 2, 4}, {4, 5, 6}, {10, 8, -12}};
 
         int result = diagonalDifference(a);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        System.out.print(String.valueOf(result));
     }
 }
 
